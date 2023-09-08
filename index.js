c692dcfd38ea49771c194cf7c0e8b8e201182982
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2023-09-08 14:43:01 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+window.addEventListener("load",checkInternetConnection);
+
+function checkInternetConnection() {
+  const statusText=document.getElementById('statusText');
+  const ipAddressText=document.getElementById('ipAddressText');
+  const networkStrengthText=document.getElementById('networkStrengthText');
+  // statusText.textContent='Checking...';
+  if(navigator.onLine){
+    // https://www.ipify.org/
+     fetch('https://api.ipify.org?format=json')
+     .then((response)=>response.json())
+     .then((data)=>{
+      ipAddressText.textContent=data.ip;
+      statusText.textContent='Connected';
+      const connection=navigator.connection;
+      const networkStrength=connection?connection.downlink +'Mbps' :'Unknown';
+       networkStrengthText.textContent=networkStrength;
+
+     })
+     .catch(()=>{
+      statusText.textContent='Disconnected';
+      ipAddressText.textConnect='-';
+      networkStrengthText.textContent='-';
+  })
+  }
+}
